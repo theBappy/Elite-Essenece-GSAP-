e436b584dd5fc7dc3a31992c4c9ff633acbc1520
theBappy <abappy575@gmail.com>
2025-06-29 23:15:28 +0600

    project setup with slice machine of prismic and created the first section as herro

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,35 @@
+import { FC } from "react";
+import { Content } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero: FC<HeroProps> = ({ slice }) => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <PrismicRichText field={slice.primary.heading} />
+      <PrismicRichText field={slice.primary.body} />
+      {slice.primary.button.map((link) => (
+  <PrismicNextLink
+    key={link.key}
+    field={link}
+    className={link.variant}
+  />
+))}
+<PrismicNextImage field={slice.primary.image} />
+
+    </section>
+  );
+};
+
+export default Hero;
